ab95788f045387dc4c54c7a3ed9569bf1e9cb9a7
Vladislav Kheynov <lcastory86@gmail.com>
2021-10-18 22:35:28 +0700

    refactor: change shapes values

--- app/src/main/java/ru/kheynov/compgraphlab5/PolygonsShapes.kt
+++ app/src/main/java/ru/kheynov/compgraphlab5/PolygonsShapes.kt
@@ -1,7 +1,9 @@
 package ru.kheynov.compgraphlab5
 
-enum class PolygonsShapes(points: Array<Array<Int>>, id: Int) {
-    STAR(id = 0, points = arrayOf(arrayOf(0, 0), arrayOf(1, 1), arrayOf(1, 2))),
-    SQUARE(id = 1, points = arrayOf(arrayOf(0, 0), arrayOf(1, 1), arrayOf(1, 2))),
-    TRIANGLE(id = 2, points = arrayOf(arrayOf(0, 0), arrayOf(1, 1), arrayOf(1, 2)))
+import androidx.compose.ui.geometry.Offset
+
+enum class PolygonsShapes(val points: List<Offset>) {
+    STAR(points = listOf(Offset(5f, 5f), Offset(1f, 1f), Offset(1f, 2f))),
+    SQUARE(points = listOf(Offset(5f, 5f), Offset(10f, 5f), Offset(10f, 10f), Offset(5f, 10f))),
+    TRIANGLE(points = listOf(Offset(0f, 0f), Offset(1f, 1f), Offset(1f, 2f)));
 }
