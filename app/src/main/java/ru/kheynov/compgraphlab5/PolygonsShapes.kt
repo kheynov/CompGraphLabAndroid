5f1b3c89683b806a2faf0a13a1ab0758224a3d05
Vladislav Kheynov <lcastory86@gmail.com>
2021-10-20 00:26:05 +0700

    feat!: delete STAR shape

--- app/src/main/java/ru/kheynov/compgraphlab5/PolygonsShapes.kt
+++ app/src/main/java/ru/kheynov/compgraphlab5/PolygonsShapes.kt
@@ -3,20 +3,6 @@ package ru.kheynov.compgraphlab5
 import androidx.compose.ui.geometry.Offset
 
 enum class PolygonsShapes(val points: List<Offset>) {
-    STAR(points = listOf(
-        Offset(0.5f, 6f),
-        Offset(3.5f, 6f),
-        Offset(5.5f, 3f),
-        Offset(7.5f, 6f),
-        Offset(10.5f, 6f),
-        Offset(7.5f, 9f),
-        Offset(10.5f, 12f),
-        Offset(7.5f, 12f),
-        Offset(5.5f, 15f),
-        Offset(3.5f, 12f),
-        Offset(0.5f, 12f),
-        Offset(3.5f, 9f)
-    )),
     SQUARE(points = listOf(
         Offset(3f, 5f),
         Offset(8f, 5f),
