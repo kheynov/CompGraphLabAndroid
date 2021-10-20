638bd58265f4c64919c9b9d015a4925adc46c56c
Vladislav Kheynov <lcastory86@gmail.com>
2021-10-20 14:07:59 +0700

    feat: add another shape

--- app/src/main/java/ru/kheynov/compgraphlab5/PolygonsShapes.kt
+++ app/src/main/java/ru/kheynov/compgraphlab5/PolygonsShapes.kt
@@ -2,24 +2,42 @@ package ru.kheynov.compgraphlab5
 
 
 enum class PolygonsShapes(val points: Array<Array<Float>>) {
-    SQUARE(points = arrayOf(
-        arrayOf(3f, 5f),
-        arrayOf(8f, 5f),
-        arrayOf(8f, 10f),
-        arrayOf(3f, 10f)
-    )
+    SQUARE(
+        points = arrayOf(
+            arrayOf(3f, 5f),
+            arrayOf(8f, 5f),
+            arrayOf(8f, 10f),
+            arrayOf(3f, 10f)
+        )
     ),
-    TRIANGLE(points = arrayOf(
-        arrayOf(4f, 2f),
-        arrayOf(8f, 6f),
-        arrayOf(5f, 10f)
-    )),
-    SOME_POLYGON(points = arrayOf(
-        arrayOf(2f, 8f),
-        arrayOf(4.5f, 9f),
-        arrayOf(6f, 6f),
-        arrayOf(9f, 9f),
-        arrayOf(5f, 13f),
-        arrayOf(3f, 12f)
-    )),
+    REVERSED_K(
+        points = arrayOf(
+            arrayOf(4f, 4f),
+            arrayOf(7f, 4f),
+            arrayOf(8f, 8f),
+            arrayOf(7f, 12f),
+            arrayOf(5f, 10f),
+            arrayOf(2f, 12f),
+            arrayOf(5f, 7f),
+            arrayOf(3f, 5f),
+            arrayOf(6f, 6f)
+        )
+    ),
+    TRIANGLE(
+        points = arrayOf(
+            arrayOf(4f, 2f),
+            arrayOf(8f, 6f),
+            arrayOf(5f, 10f)
+        )
+    ),
+    SOME_POLYGON(
+        points = arrayOf(
+            arrayOf(2f, 8f),
+            arrayOf(4.5f, 9f),
+            arrayOf(6f, 6f),
+            arrayOf(9f, 9f),
+            arrayOf(5f, 13f),
+            arrayOf(3f, 12f)
+        )
+    ),
 }
